76eeb37bad3d73acfd864635324c0d6cd36fa8be
Francis Schlimmer <francis.schlimmer@gmail.com>
2010-10-12 03:03:11 -0400

    working on line list rendering

--- TankGame/TankGame/TankGame/Game1.cs
+++ TankGame/TankGame/TankGame/Game1.cs
@@ -18,6 +18,11 @@ namespace TankGame
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        VertexPositionColor[] pointList;
+        Matrix viewMatrix;
+        Matrix projectionMatrix;
+        static int points = 8;
+        short[] lineListIndices;
 
         public Game1()
         {
@@ -34,7 +39,9 @@ namespace TankGame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            InitializePoints();
+            InitializeIndicies();
+            InitializeCamera();
             base.Initialize();
         }
 
@@ -47,7 +54,10 @@ namespace TankGame
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+
             // TODO: use this.Content to load your game content here
+
+
         }
 
         /// <summary>
@@ -75,6 +85,7 @@ namespace TankGame
             base.Update(gameTime);
         }
 
+
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
@@ -82,10 +93,67 @@ namespace TankGame
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.DrawUserIndexedPrimitives<VertexPositionColor>(
+            PrimitiveType.LineList,
+            pointList,
+            0,  // vertex buffer offset to add to each element of the index buffer
+            8,  // number of vertices in pointList
+            lineListIndices,  // the index buffer
+            0,  // first index element to read
+            7   // number of primitives to draw
+            );
 
-            // TODO: Add your drawing code here
 
+            // Create effects and set properties
             base.Draw(gameTime);
+
         }
+
+        private void InitializePoints()
+        {
+
+            pointList = new VertexPositionColor[points];
+            for (int x = 0; x < points / 2; x++)
+            {
+                for (int y = 0; y < 2; y++)
+                {
+                    pointList[(x * 2) + y] = new VertexPositionColor(
+                        new Vector3(x * 100, y * 100, 0), Color.White);
+                }
+            }
+
+        }
+
+        private void InitializeCamera()
+        {
+            viewMatrix = Matrix.CreateLookAt(
+                new Vector3(0.0f, 0.0f, 1.0f),
+                Vector3.Zero,
+                Vector3.Up
+                );
+
+            projectionMatrix = Matrix.CreateOrthographicOffCenter(
+                0,
+                (float)GraphicsDevice.Viewport.Width,
+                (float)GraphicsDevice.Viewport.Height,
+                0,
+                1.0f, 1000.0f);
+
+
+        }
+
+        private void InitializeIndicies()
+        {
+            // Initialize an array of indices of type short.
+            lineListIndices = new short[(points * 2) - 2];
+
+            // Populate the array with references to indices in the vertex buffer
+            for (int i = 0; i < points - 1; i++)
+            {
+                lineListIndices[i * 2] = (short)(i);
+                lineListIndices[(i * 2) + 1] = (short)(i + 1);
+            }
+        }
+
     }
 }
