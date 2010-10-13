941a3793dde54288f47e1ce50ba44a81f2efcd2e
Francis Schlimmer <francis.schlimmer@gmail.com>
2010-10-12 21:05:38 -0400

    messign with triangles

--- TankGame/TankGame/TankGame/Game1.cs
+++ TankGame/TankGame/TankGame/Game1.cs
@@ -18,7 +18,9 @@ namespace TankGame
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        BasicEffect basicEffect;
+        VertexPositionColor[] box;
+        VertexPositionColor[] circle;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -33,8 +35,40 @@ namespace TankGame
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            basicEffect = new BasicEffect(graphics.GraphicsDevice);
+            basicEffect.VertexColorEnabled = true;
+            basicEffect.Projection = Matrix.CreateOrthographicOffCenter
+               (0, graphics.GraphicsDevice.Viewport.Width,     // left, right
+                graphics.GraphicsDevice.Viewport.Height, 0,    // bottom, top
+                0, 1);                                         // near, far plane
 
+            box = new VertexPositionColor[12];
+
+            box[0] = new VertexPositionColor(new Vector3(10, 50, 0), Color.Red);
+            box[1] = new VertexPositionColor(new Vector3(10, 10, 0), Color.Green);
+            box[2] = new VertexPositionColor(new Vector3(50, 50, 0), Color.Blue);
+            box[3] = new VertexPositionColor(new Vector3(50, 10, 0), Color.Red);
+            box[4] = new VertexPositionColor(new Vector3(100, 50, 0), Color.Green);
+            box[5] = new VertexPositionColor(new Vector3(100, 10, 0), Color.Blue);
+            box[6] = new VertexPositionColor(new Vector3(150, 50, 0), Color.Red);
+            box[7] = new VertexPositionColor(new Vector3(150, 10, 0), Color.Green);
+            box[8] = new VertexPositionColor(new Vector3(200, 50, 0), Color.Blue);
+            box[9] = new VertexPositionColor(new Vector3(200, 10, 0), Color.Red);
+            box[10] = new VertexPositionColor(new Vector3(250, 50, 0), Color.Green);
+            box[11] = new VertexPositionColor(new Vector3(200, 10, 0), Color.Blue);
+
+
+
+
+
+            circle = new VertexPositionColor[100];
+            for (int i = 0; i < 99; i++)
+            {
+                float angle = (float)(i / 100.0 * Math.PI * 2);
+                circle[i].Position = new Vector3(400 + (float)Math.Cos(angle) * 100, 200 + (float)Math.Sin(angle) * 100, 0);
+                circle[i].Color = Color.Green;
+            }
+            circle[99] = circle[0];
             base.Initialize();
         }
 
@@ -77,14 +111,25 @@ namespace TankGame
 
         /// <summary>
         /// This is called when the game should draw itself.
-        /// </summary>
+        /// </summary2
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-
+            basicEffect.CurrentTechnique.Passes[0].Apply();
+            Vector3 scale = new Vector3(50, 50, 0);
+            VertexPositionColor[] temp = new VertexPositionColor[12];
+            Array.Copy(box, temp, 12);
+            for (int i = 0; i < 5; i++)
+            {
+                graphics.GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleStrip, temp, 0, 10);
+                for (int j = 1; j < 12; j++)
+                {
+                    temp[j].Position += scale;
+                }
+            }
+            graphics.GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.LineStrip, circle, 0, 99);
             base.Draw(gameTime);
         }
     }
